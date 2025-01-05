0b058ce57ae75ec4fc05b10f0eee660b32c916c0
Arnaud Somville <105423438+arnaudsomville@users.noreply.github.com>
2025-01-05 22:47:46 +0100

    Update run_povray_container.sh

--- vapory/docker_container/run_povray_container.sh
+++ vapory/docker_container/run_povray_container.sh
@@ -6,9 +6,18 @@ POVRAY_WIDTH=$3
 POVRAY_HEIGHT=$4
 POVRAY_EXTRA_PARAMS=$5
 
+# Chemin du fichier docker-compose.yml
+DOCKER_COMPOSE_FILE="$(dirname "$0")/docker-compose.yml"
+
 # Vérifiez si les arguments sont fournis
 if [[ -z "$POVRAY_INPUT_FILE" || -z "$POVRAY_RESOURCES" || -z "$POVRAY_WIDTH" || -z "$POVRAY_HEIGHT" || -z "$POVRAY_EXTRA_PARAMS" ]]; then
   echo "Usage: $0 <input_file> <resources> <width> <height> <extra_params>"
+  exit 1
+fi
+
+# Vérifiez si le fichier docker-compose.yml existe
+if [[ ! -f "$DOCKER_COMPOSE_FILE" ]]; then
+  echo "Erreur : Fichier docker-compose.yml introuvable à l'emplacement : $DOCKER_COMPOSE_FILE"
   exit 1
 fi
 
@@ -19,5 +28,5 @@ export POVRAY_WIDTH
 export POVRAY_HEIGHT
 export POVRAY_EXTRA_PARAMS
 
-# Lancer docker-compose
-docker-compose up
+# Lancer docker-compose avec le fichier spécifié
+docker-compose -f "$DOCKER_COMPOSE_FILE" up
